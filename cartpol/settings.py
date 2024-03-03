e8a656447e63480feec46e8c2c1196d3098cf2e9
iuri.felix <iuri.felix@BRSPPM-DKQM4VQGJY.local>
2024-03-03 16:07:32 -0300

    feat: remove cache s3

--- cartpol/settings.py
+++ cartpol/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
