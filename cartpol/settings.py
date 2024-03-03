46894829fa30978d0e1914ed1c84ee6d36d49d42
iuri.felix <iuri.felix@BRSPPM-DKQM4VQGJY.local>
2024-03-03 15:52:20 -0300

    feat: add static root

--- cartpol/settings.py
+++ cartpol/settings.py
@@ -146,9 +146,10 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
-WHITENOISE_USE_FINDERS = True
-WHITENOISE_MANIFEST_STRICT = False
+# WHITENOISE_USE_FINDERS = True
+# WHITENOISE_MANIFEST_STRICT = False
 WHITENOISE_ALLOW_ALL_ORIGINS = True
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
