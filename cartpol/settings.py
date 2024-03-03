4c4e016d88e6a3a6a0766be21684f91c8f79b38b
iuri.felix <iuri.felix@BRSPPM-DKQM4VQGJY.local>
2024-03-03 15:26:06 -0300

    feat: remove requirements

--- cartpol/settings.py
+++ cartpol/settings.py
@@ -30,11 +30,9 @@ IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 # # SECURITY WARNING: don't run with debug turned on in production!
 if not IS_HEROKU_APP:
     DEBUG = True
-
-if IS_HEROKU_APP:
-    ALLOWED_HOSTS = ["*"]
-else:
     ALLOWED_HOSTS = []
+
+
 PORT = os.getenv("PORT", default="8000")
 # # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -86,20 +84,7 @@ django_heroku.settings(locals())
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if IS_HEROKU_APP:
-    # In production on Heroku the database configuration is derived from the `DATABASE_URL`
-    # environment variable by the dj-database-url package. `DATABASE_URL` will be set
-    # automatically by Heroku when a database addon is attached to your Heroku app. See:
-    # https://devcenter.heroku.com/articles/provisioning-heroku-postgres
-    # https://github.com/jazzband/dj-database-url
-    DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
-    }
-else:
+if not IS_HEROKU_APP:
     # When running locally in development or in CI, a sqlite database file will be used instead
     # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
     DATABASES = {
