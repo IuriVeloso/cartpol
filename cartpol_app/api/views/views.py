f6d91d2f7c1fc84db86a9184a4a71a711b72f824
IuriVeloso <iuriv.felix@gmail.com>
2024-07-16 09:53:06 -0300

    feat: add locals cache

--- cartpol_app/api/views/views.py
+++ cartpol_app/api/views/views.py
@@ -10,7 +10,6 @@ from cartpol_app.api.serializers import (CountySerializer, ElectionSerializer,
                                          PoliticalSerializer,
                                          PoliticalTypeSerializer,
                                          SectionSerializer, StateSerializer,
-                                         VotesResultSerializer,
                                          VotesSerializer)
 from cartpol_app.models import (County, Election, ElectoralZone, Neighborhood,
                                 Political, PoliticalParty, PoliticalType,
@@ -20,6 +19,9 @@ from cartpol_app.models import (County, Election, ElectoralZone, Neighborhood,
 class StateAV(APIView):
     def get(self, request):
         states = State.objects.all()
+        should_search_state = request.query_params.get('name', False)
+        if should_search_state:
+            states = states.filter(name=should_search_state)
         state_serializer = StateSerializer(states, many=True)
         return Response(state_serializer.data, status=status.HTTP_200_OK)
 
@@ -56,12 +58,22 @@ class CountyAV(APIView):
 class NeighborhoodAV(APIView):
     def get(self, request):
         neighborhoods = Neighborhood.objects.all()
+        should_search_neighborhood = request.query_params.get('name', False)
+        should_search_county = request.query_params.get('county', False)
+        if should_search_neighborhood:
+            neighborhoods = neighborhoods.filter(
+                name=should_search_neighborhood)
+        if should_search_county:
+            neighborhoods = neighborhoods.filter(
+                county__name=should_search_county)
+
         neighborhood_serializer = NeighborhoodSerializer(
             neighborhoods, many=True)
         return Response(neighborhood_serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        neighborhood_serializer = NeighborhoodSerializer(data=request.data)
+        neighborhood_serializer = NeighborhoodSerializer(
+            data=request.data, many=isinstance(request.data, list))
         if neighborhood_serializer.is_valid():
             neighborhood_serializer.save()
             return Response(neighborhood_serializer.data, status=status.HTTP_201_CREATED)
@@ -86,6 +98,17 @@ class CountysNeighborhoodAV(APIView):
 class ElectoralZoneAV(APIView):
     def get(self, request):
         electoral_zones = ElectoralZone.objects.all()
+        should_search_electoral_zone = request.query_params.get(
+            'identifier', False)
+        should_search_county = request.query_params.get('county', False)
+
+        if should_search_electoral_zone:
+            electoral_zones = electoral_zones.filter(
+                identifier=should_search_electoral_zone)
+        if should_search_county:
+            electoral_zones = electoral_zones.filter(
+                county__name=should_search_county)
+
         electoral_zone_serializer = ElectoralZoneSerializer(
             electoral_zones, many=True)
         return Response(electoral_zone_serializer.data, status=status.HTTP_200_OK)
@@ -116,6 +139,12 @@ class PoliticalTypeAV(APIView):
 class PoliticalPartyAV(APIView):
     def get(self, request):
         political_parties = PoliticalParty.objects.all()
+
+        should_search_party = request.query_params.get('name', False)
+        if should_search_party:
+            political_parties = political_parties.filter(
+                name=should_search_party)
+
         political_party_serializer = PoliticalPartySerializer(
             political_parties, many=True)
         return Response(political_party_serializer.data, status=status.HTTP_200_OK)
@@ -146,19 +175,22 @@ class ElectionAV(APIView):
 class PoliticalAV(APIView):
     def get(self, request):
         politicals = Political.objects.all()
-        
-        should_search_political_code = request.query_params.get('political_code', False)
+
+        should_search_political_code = request.query_params.get(
+            'political_code', False)
         should_search_full_name = request.query_params.get('full_name', False)
         if should_search_full_name:
             politicals = politicals.filter(full_name=should_search_full_name)
         if should_search_political_code:
-            politicals = politicals.filter(political_code=should_search_political_code)
-        
+            politicals = politicals.filter(
+                political_code=should_search_political_code)
+
         political_serializer = PoliticalSerializer(politicals, many=True)
         return Response(political_serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        political_serializer = PoliticalSerializer(data=request.data)
+        political_serializer = PoliticalSerializer(
+            data=request.data, many=isinstance(request.data, list))
         if political_serializer.is_valid():
             political_serializer.save()
             return Response(political_serializer.data, status=status.HTTP_201_CREATED)
@@ -172,7 +204,8 @@ class VotesAV(APIView):
         return Response(votes_serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        votes_serializer = VotesSerializer(data=request.data)
+        votes_serializer = VotesSerializer(
+            data=request.data, many=isinstance(request.data, list))
         if votes_serializer.is_valid():
             votes_serializer.save()
             return Response(votes_serializer.data, status=status.HTTP_201_CREATED)
@@ -182,22 +215,27 @@ class VotesAV(APIView):
 class SectionAV(APIView):
     def get(self, request):
         sections = Section.objects.all()
-        
+
         should_search_county = request.query_params.get('county', False)
-        should_search_electoral_zone = request.query_params.get('electoral_zone', False)
-        should_search_identifier = request.query_params.get('identifier', False)
+        should_search_electoral_zone = request.query_params.get(
+            'electoral_zone', False)
+        should_search_identifier = request.query_params.get(
+            'identifier', False)
         if should_search_county:
-            sections = sections.filter(neighborhood__county__name=should_search_county)
+            sections = sections.filter(
+                neighborhood__county__name=should_search_county)
         if should_search_electoral_zone:
-            sections = sections.filter(electoral_zone__identifier=should_search_electoral_zone)
+            sections = sections.filter(
+                electoral_zone__identifier=should_search_electoral_zone)
         if should_search_identifier:
             sections = sections.filter(identifier=should_search_identifier)
-        
+
         section_serializer = SectionSerializer(sections, many=True)
         return Response(section_serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        section_serializer = SectionSerializer(data=request.data)
+        section_serializer = SectionSerializer(
+            data=request.data, many=isinstance(request.data, list))
         if section_serializer.is_valid():
             section_serializer.save()
             return Response(section_serializer.data, status=status.HTTP_201_CREATED)
@@ -214,15 +252,14 @@ class PoliticalVotesAV(APIView):
 
         political = Political.objects.get(id=int(political_id))
 
-        print(total_candidate_votes.__len__())
-        print(political)
-
         total_votes = Votes.objects.filter(
             political_id=int(political_id)).aggregate(Sum('quantity'))
         total_neighborhoods_votes = Votes.objects.values(
             'section__neighborhood').annotate(total=Sum('quantity'))
 
         total_political_votes = total_votes.get("quantity__sum")
+
+        print(political, total_political_votes)
 
         votes_by_neighborhood = []
 
--- cartpol_app/models.py
+++ cartpol_app/models.py
@@ -1,86 +1,103 @@
-from django.db import models
 from django.core.exceptions import ValidationError
+from django.db import models
 
 # Create your models here.
+
 
 def validate_list(value):
     if value not in ['federal', 'state', 'city']:
         raise ValidationError(
             ('%(value)s must be one of "["federal", "state", "city"]"'),
             params={'value': value}
-            )
+        )
+
 
 class State (models.Model):
     name = models.CharField(max_length=8)
     full_name = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
+
 
 class County (models.Model):
     name = models.CharField(max_length=40)
     state = models.ForeignKey(State, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
-    
+
+
 class Neighborhood (models.Model):
     name = models.CharField(max_length=100)
     county = models.ForeignKey(County, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
-    
+
+
 class ElectoralZone (models.Model):
     identifier = models.CharField(max_length=40)
-    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    county = models.ForeignKey(County, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.identifier
-    
+
+
 class Section (models.Model):
     identifier = models.CharField(max_length=40)
     cep = models.CharField(max_length=10, default='')
     address = models.CharField(max_length=100, default='')
     electoral_zone = models.ForeignKey(ElectoralZone, on_delete=models.CASCADE)
     neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.identifier
+
 
 class PoliticalType (models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=200)
+
     def __str__(self):
         return self.name
+
 
 class PoliticalParty (models.Model):
     name = models.CharField(max_length=40)
     full_name = models.CharField(max_length=200)
     active = models.BooleanField(default=True)
-    
+
     def __str__(self):
         return self.name
+
 
 class Election (models.Model):
     year = models.IntegerField()
     round = models.IntegerField()
     code = models.IntegerField()
 
+
 class Political (models.Model):
     name = models.CharField(max_length=100)
     full_name = models.CharField(max_length=200)
-    political_party = models.ForeignKey(PoliticalParty, on_delete=models.CASCADE)
+    political_party = models.ForeignKey(
+        PoliticalParty, on_delete=models.CASCADE)
     political_type = models.ForeignKey(PoliticalType, on_delete=models.CASCADE)
     election = models.ForeignKey(Election, on_delete=models.CASCADE)
     region = models.CharField(max_length=50, validators=[validate_list])
-    region_id=models.IntegerField()
+    region_id = models.IntegerField()
     political_code = models.IntegerField()
-    
+
     def __str__(self):
         return self.name
-    
+
+
 class Votes (models.Model):
     quantity = models.IntegerField()
     political = models.ForeignKey(Political, on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    
+
     def __votes__(self):
         return self.quantity
-    
