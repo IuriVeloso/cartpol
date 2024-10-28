08a5c5c3d4734a1e9a607e7e6a6ec22f0dbf1fd8
IuriVeloso <iuriv.felix@poli.ufrj.br>
2024-10-28 10:55:51 -0300

    test: remove cors whitelist

--- cartpol/settings.py
+++ cartpol/settings.py
@@ -64,7 +64,6 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    'http://localhost:3001',
     'http://192.168.0.16:3000',
     'https://cartpol-de4ebf8415a8.herokuapp.com',
     'https://cartpol.lappcom.com.br',
