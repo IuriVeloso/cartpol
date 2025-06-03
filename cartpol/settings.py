6bac81bce6952e310f32a1a8c9f55387a221a71f
IuriVeloso <iuriv.felix@poli.ufrj.br>
2025-06-03 14:42:38 -0300

    docs: edit readme

--- cartpol/settings.py
+++ cartpol/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-import dj_database_url  # type: ignore
 import django_heroku  # type: ignore
 from dotenv import load_dotenv
 
